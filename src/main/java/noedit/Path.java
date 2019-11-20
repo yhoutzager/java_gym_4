1ec422c5d151a0e84f20949da5edb3970e71d800
Mark <mverleg.noreply@gmail.com>
2019-11-20 19:21:30 +0100

    Validate physical possibility of paths

--- src/main/java/noedit/Path.java
+++ src/main/java/noedit/Path.java
@@ -1,13 +1,17 @@
 package noedit;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 
 import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.Validate;
+import org.checkerframework.checker.index.qual.NonNegative;
+import org.checkerframework.common.value.qual.IntRange;
 import org.checkerframework.common.value.qual.MinLen;
 
 /**
@@ -33,14 +37,44 @@ public final class Path {
 	 * </ul>
 	 */
 	public boolean isPhysical() {
-		if (1==1) throw new NotImplementedException("todo: ");  //TODO @mark:
-
+		if (validatedPhysical) {
+			return true;
+		}
+		Set<Position> seen = new HashSet<>();
+		Position prev = first();
+		for (int i = 1; i < size(); i++) {
+			Position current = get(i);
+			if (seen.contains(current)) {
+				System.err.println("Warning: move #" + i + " revisits position " + current + " which was already visited");
+				// This must be a valid position, since it was visited before.
+			} else if (current.t < prev.t) {
+				System.err.println("Illegal move (#" + i + ", " + current + ") — cannot travel back in time");
+				return false;
+			} else if (current.t > prev.t + 1) {
+				System.err.println("Illegal move (#" + i + ", " + current + ") — cannot move than one timestep forward per turn");
+				return false;
+			} else if (Math.abs(current.x - prev.x) > 1) {
+				System.err.println("Illegal move (#" + i + ", " + current + ") — cannot travel more than one step horizontally");
+				return false;
+			} else if (Math.abs(current.y - prev.y) > 1) {
+				System.err.println("Illegal move (#" + i + ", " + current + ") — cannot travel more than one step vertically");
+				return false;
+			} else if (current.x != prev.x && current.y != prev.y) {
+				System.err.println("Illegal move (#" + i + ", " + current + ") — cannot travel both horizontally and vertically in the same turn");
+				return false;
+			} else if (current.t != prev.t && (current.x != prev.x || current.y != prev.y)) {
+				System.err.println("Illegal move (#" + i + ", " + current + ") — cannot travel both spatially and advance to the next timestep in the same turn");
+				return false;
+			}
+			seen.add(current);
+			prev = current;
+		}
 		validatedPhysical = true;
 		return true;
 	}
 
 	public boolean isSolution(@Nonnull Maze maze) {
-		Validate.isTrue(validatedPhysical);
+		Validate.isTrue(isPhysical());
 
 		if (1==1) throw new NotImplementedException("todo: ");  //TODO @mark:
 		return true;
@@ -56,5 +90,16 @@ public final class Path {
 	@CheckReturnValue
 	public Position last() {
 		return steps.get(steps.size() - 1);
+	}
+
+	@Nonnull
+	@CheckReturnValue
+	public Position get(@NonNegative int index) {
+		return steps.get(index);
+	}
+
+	@IntRange(from = 0)
+	public int size() {
+		return steps.size();
 	}
 }
