23d28a6ef6ff0f18ce39f0518185222a3a97d01b
Mark <mverleg.noreply@gmail.com>
2019-11-23 11:58:29 +0100

    Make the performance test a bit more balanced based on results so far

--- src/test/java/javagym/SolutionTest.java
+++ src/test/java/javagym/SolutionTest.java
@@ -602,16 +602,20 @@ public class SolutionTest {
     }
 
     @Test
-    void testPerformance() {
-        double total = 0;
+    void testPerformanceV2() {
+        // Hint: Do not run in debug mode!
+        double perfectTotal = 0.0;
         for (int i = 0; i < 50; i++) {
-            Pair<Maze, Position> puzzle = MazeGenerator.generate(246_800_000 + 111 * i, 5, 150, 0.0, 1);
-            total += checkMazeSolution(puzzle.getLeft(), puzzle.getRight());
+            Pair<Maze, Position> puzzle = MazeGenerator.generate(246_800_000 + 111 * i, 6, 150, 0.0, 1);
+            perfectTotal += checkMazeSolution(puzzle.getLeft(), puzzle.getRight());
         }
+        System.out.println(String.format("perfect took %.3f ms", perfectTotal));
+        double porousTotal = 0.0;
         for (int i = 0; i < 100; i++) {
-            Pair<Maze, Position> puzzle = MazeGenerator.generate(135_780_000 + 111 * i, 6, 150, 0.10, 5);
-            total += checkMazeSolution(puzzle.getLeft(), puzzle.getRight());
+            Pair<Maze, Position> puzzle = MazeGenerator.generate(135_780_000 + 111 * i, 10, 150, 0.10, 5);
+            porousTotal += checkMazeSolution(puzzle.getLeft(), puzzle.getRight());
         }
-        System.out.println(String.format("took %.3f ms", total));
+        System.out.println(String.format("porous took %.3f ms", porousTotal));
+        System.out.println(String.format("total took %.3f ms", perfectTotal + porousTotal));
     }
 }
